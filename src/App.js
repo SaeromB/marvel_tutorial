f6cfa05d8d5dc020755447da953babd90c7378e5
Saerom <saerombang11@gmail.com>
2019-08-26 17:59:01 +0900

    added CharacterList.js and Route path

--- src/App.js
+++ src/App.js
@@ -9,57 +9,27 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Character from './Character';
+import CharacterList from './CharacterList';
 
-
-class App extends Component {
-  state = {
-    isLoading: true,
-    characters: [],
-    selectedCharacterUrl: null,
-    selectedCharacterData: null,
-  };
-  getCharacters= async()=>{
-    const res = await fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5d2cbb76e14fbe42f05ec010936c4542&hash=ffe8962d76a12c5db0198cd0c503fa6c');
-    const {data:{results}} = await res.json()
-    this.setState({characters:results, isLoading:false})
-    console.log(results);
-  }
-  componentDidMount(){
-    this.getCharacters();
-  }
-
-  render(){
-    const {isLoading, characters} = this.state;
-    return(
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Route path="/comicBook" component={comicBook}/>
-          <div>{isLoading ? 'Loading...': characters.map(character=>{
-          // always return something after map
-          return <Character
-                  key={character.id}
-                  id={character.id}
-                  name={character.name}
-                  description={character.description}
-                  thumbnail={character.thumbnail}
-                  comics={character.comics}/>}
-          )}
-          </div>
-        </div>
-      </Router>
-    )
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route path="/" component={CharacterList}/>
+        <Route path="/test" component={CharacterList}/>
+      </Switch>
+    </div>
+  </Router>
+);
 
 
 export default App;
 
-const comicBook = () => (
-  <h1>comicBook</h1>
+const test = () => (
+  <h1>test</h1>
 )
 
 
---
+++ src/CharacterList.js
@@ -0,0 +1,48 @@
+
+import React, {Component} from 'react';
+import Character from './Character';
+
+
+class CharacterList extends Component {
+  state = {
+    isLoading: true,
+    characters: [],
+    selectedCharacterUrl: null,
+    selectedCharacterData: null,
+  };
+  
+  getCharacters= async()=>{
+    const res = await fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5d2cbb76e14fbe42f05ec010936c4542&hash=ffe8962d76a12c5db0198cd0c503fa6c');
+    const {data:{results}} = await res.json()
+    this.setState({characters:results, isLoading:false})
+    console.log(results);
+  }
+  componentDidMount(){
+    this.getCharacters();
+  }
+  
+
+  render(){
+    const {isLoading, characters} = this.state;
+    return(      
+          <div>{isLoading ? 'Loading...': characters.map(character=>{
+          // always return something after map
+          return <Character
+                  key={character.id}
+                  id={character.id}
+                  name={character.name}
+                  description={character.description}
+                  thumbnail={character.thumbnail}
+                  comic={character.comic}
+                  />}
+          )}
+          </div>
+    )
+  }
+}
+
+
+export default CharacterList;
+
+
+
