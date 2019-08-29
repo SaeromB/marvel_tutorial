66a8037e4009c07c4bbb412acfe9b5ab94d78d1a
Saerom <saerombang11@gmail.com>
2019-08-29 18:15:11 +0900

    changed CharacterList.js

--- src/Character.js
+++ src/Character.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -8,8 +7,10 @@ const Character=({id, name, description, thumbnail, comics})=>{
         <div>
             <Link to = {`/${id}`}>
             <h1 className='character_name'>{name}</h1>
-            <img src={`${(thumbnail.path)+'/portrait_xlarge.jpg'}` } alt={Character.name}/>
+            <img src={`${(thumbnail.path)+'/portrait_xlarge.jpg'}` } alt={name}/>
             {/* <img src={`${thumbnail.path}.${thumbnail.extension})`} alt={Character.name}></img> */}
+            <p>{description}</p>
+            <p>{comics}</p>
             </Link>
         </div>
     )
@@ -19,7 +20,7 @@ Character.propTypes = {
     id:PropTypes.number.isRequired,
     name:PropTypes.string.isRequired,
     description:PropTypes.string.isRequired,
-    thumbnail:PropTypes.string.isRequired,
+    thumbnail:PropTypes.object.isRequired,
     comics:PropTypes.string.isRequired,
 }
 
--- src/CharacterList.js
+++ src/CharacterList.js
@@ -11,30 +11,45 @@ class CharacterList extends Component {
   getCharacters= async()=>{
     const res = await fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5d2cbb76e14fbe42f05ec010936c4542&hash=ffe8962d76a12c5db0198cd0c503fa6c');
     const {data:{results}} = await res.json()
-    this.setState({characters:results, isLoading:false})
+    this.setState({characters:results})
     console.log(results);
   }
   componentDidMount(){
     this.getCharacters();
   }
   
-
   render(){
-    const {isLoading, characters} = this.state;
-    return(      
-          <div>{isLoading ? 'Loading...': characters.map(character=>{
-          // always return something after map
-          return <Character
-                  key={character.id}
-                  id={character.id}
-                  name={character.name}
-                  thumbnail={character.thumbnail}
-                  />}
-          )}
-          </div>
+    const {characters} = this.state
+   return(
+    <div>
+      {characters.map(character => 
+        <Character key={character.id} 
+        id={character.id} 
+        name={character.name} 
+        thumbnail={character.thumbnail}/>)}
+    </div>
+
     )
   }
 }
 
-
 export default CharacterList;
+
+  // 
+  // render(){
+  //   const {isLoading, characters} = this.state;
+  //   return(      
+  //         <div>{isLoading ? 'Loading...': characters.map(character=>{
+  //         // always return something after map
+  //         return <Character
+  //                 key={character.id}
+  //                 id={character.id}
+  //                 name={character.name}
+  //                 thumbnail={character.thumbnail}
+  //                 />}
+  //         )}
+  //         </div>
+  //  
+
+
+
